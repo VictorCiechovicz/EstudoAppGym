0cdbd3e21aa2c2889642e114fd8cc4903ce9728d
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-07-26 21:50:23 -0300

    upgrade

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {View, Text} from 'react-native';
+
 import TelaInicial from './Componentes/TelaInicial/index';
 import TelaLogin from './Componentes/TelaLogin/index';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View>
-      <Text>Teste</Text>
-      <Text>Teste</Text>
-      <Text>Teste</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={TelaInicial} />
+        <Stack.Screen name="Login" component={TelaLogin} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
